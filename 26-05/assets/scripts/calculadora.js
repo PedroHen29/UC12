2fbd273269958da5dcd70dd13344975721cd8852
PedroHen29 <pedroeichamaral@gmail.com>
2026-05-30 23:26:47 -0300

    Add files via upload

--- 26-05/assets/scripts/calculadora.js
+++ 26-05/assets/scripts/calculadora.js
@@ -2,6 +2,8 @@ const numeros = document.querySelectorAll(".numero")
 const visor = document.getElementById("h1")
 const operacoes = document.querySelectorAll(".operacao")
 
+
+
 let primeiroNumero = ""
 let segundoNumero = ""
 let operador = ""
@@ -13,7 +15,7 @@ numeros.forEach(botao => {
             visor.textContent = primeiroNumero
         }else{
             segundoNumero += botao.textContent;
-            visor.textContent = segundoNumero;
+            visor.textContent = primeiroNumero+operador+segundoNumero;
         }
     })
 })
@@ -21,6 +23,7 @@ numeros.forEach(botao => {
 document.getElementById("mais").addEventListener("click", () =>{
     operador = "+"
     numeroVazio = true
+    
 })
 
 document.getElementById("menos").addEventListener("click", () => {
@@ -42,9 +45,11 @@ document.getElementById("igual").addEventListener("click", () => {
 
     const num1 = Number(primeiroNumero)
     const num2 = Number(segundoNumero)
+    
     let resultado;
     switch(operador){
         case "+":
+            
             resultado = num1 + num2
             break
         case "-":
@@ -75,6 +80,7 @@ document.getElementById("igual").addEventListener("click", () => {
     numeroVazio = false
 })
 
+
 document.getElementById("limpar").addEventListener("click", () => {
     primeiroNumero = ""
     segundoNumero = ""
@@ -82,4 +88,47 @@ document.getElementById("limpar").addEventListener("click", () => {
     numeroVazio = false
 
     visor.textContent = "0"
+})
+
+const img = document.getElementById("dianoite")
+const body = document.getElementById("corpo")
+const base = document.querySelector(".base")
+const mudarTema = document.getElementById("mudarTema")
+mudarTema.addEventListener("click", () =>{
+    if(img.getAttribute("src") == "assets/images/sun.png"){
+        mudarTema.style.background = "#dfdddd"
+        mudarTema.style.border = "1px solid gray"
+        body.style.backgroundColor = "#e2dfdf"
+        base.style.backgroundColor = "#d3d4d6"
+        base.style.border = "2px solid gray"
+        
+        img.setAttribute("src", "assets/images/moon.png")
+
+        numeros.forEach(botao => {
+           botao.style.backgroundColor = "#e2dfdf" 
+           botao.style.border = "2px solid gray"
+        })
+
+        operacoes.forEach(botao =>
+            botao.style.backgroundColor = "#615ada"
+        )
+        
+        
+    }else{
+        img.setAttribute("src", "assets/images/sun.png")
+        body.style.backgroundColor = "#000000"
+        base.style.backgroundColor = "#363636"
+        base.style.border = "2px solid white"
+
+        
+
+        numeros.forEach(botao => {
+           botao.style.backgroundColor = "#4F4F4F" 
+           botao.style.border = "2px solid white"
+        })
+
+        operacoes.forEach(botao =>
+            botao.style.backgroundColor = "#EE7621"
+        )
+    }
 })
